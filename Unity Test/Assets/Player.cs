a116abc6dd97c1c00ea077e65a9f682b4c704278
hammone2 <hammone2@my.erau.edu>
2024-01-17 15:44:50 -0700

    Update Player.cs

--- Unity Test/Assets/Player.cs
+++ Unity Test/Assets/Player.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+
 
 public class Player : MonoBehaviour
 {
+
+    // declare variables
+    public float move_speed = 0.05f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,40 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // forward
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * move_speed;
+        }
+
+        // back
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * move_speed;
+        }
+
+        // left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * move_speed;
+        }
+
+        // right
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * move_speed;
+        }
+
+        //fly
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += Vector3.up * move_speed;
+        }
+
+        //fall
+        if ((Input.GetKey(KeyCode.LeftShift)) || (Input.GetKey(KeyCode.RightShift)))
+        {
+            transform.position += Vector3.down * move_speed;
+        }
     }
 }
